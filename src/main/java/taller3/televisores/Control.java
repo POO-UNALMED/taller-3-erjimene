5c743b98aca5c4fe29bf302a46aac6011a2fb6f7
Edwin Jimenez Gomez <erjimene@unal.edu.co>
2020-08-29 17:40:55 -0500

    Termine1

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,59 @@
+package taller3.televisores;
+
+public class Control {
+	
+	private TV tv;
+	
+	public void turnOn() {
+		if (tv.getEstado() == false) {
+			tv.estado = true;
+		}
+	}
+		
+	public void turnOff() {
+		if (tv.getEstado() == true) {
+			tv.estado = false;
+		}
+	}
+	
+	public void canalUp() {
+		if (tv.getCanal()<120 && tv.getEstado() == true) {
+			tv.setCanal(tv.getCanal()+1);
+		}		
+	}
+	
+	public void canalDown() {
+		if (tv.getCanal()>1 && tv.getEstado() == true) {
+			tv.setCanal(tv.getCanal()-1);
+		}		
+	}
+	
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+	
+	public void volumenUp() {
+		if (tv.getVolumen()<7 && tv.getEstado() == true) {
+			tv.setVolumen(tv.getVolumen()+1);
+		}		
+	}
+	
+	public void volumenDown() {
+		if (tv.getVolumen()>1 && tv.getEstado() == true) {
+			tv.setVolumen(tv.getVolumen()-1);
+		}
+	}
+	
+	public void enlazar(TV tv) {
+		this.tv =tv;		
+	}
+	
+	public TV getTv() {
+		return tv;
+	}
+	
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,72 @@
+package taller3.televisores;
+
+public class TV {
+	
+	private Marca marca;
+	private int canal = 1, volumen = 1, precio = 500;
+    boolean estado; // encendido true, apagado false.
+	private Control control;
+	private static int numTV;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+	}
+	
+	public Marca getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	
+	public Control getControl() {
+		return control;
+	}
+	
+	public void setControl(Control control) {
+		this.control = control;
+	}
+	
+	public int getPrecio() {
+		return precio;
+	}
+	
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	
+	public int getVolumen() {
+		return volumen;
+	}
+	
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+	
+	public int getCanal() {
+		return canal;
+	}
+	
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+	
+	public int getNumTV() {
+		return numTV;
+	}
+	
+	public void turnOn() {
+		this.estado = true;
+	}
+	
+	public void turnOff() {
+		this.estado = false;
+	}
+	
+	public boolean getEstado() {
+		return estado;
+	}
+	
+}
