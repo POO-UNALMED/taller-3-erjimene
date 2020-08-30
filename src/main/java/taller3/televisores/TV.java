9c0712c378182cac21e5ca84592e57c5a034d4e7
Edwin Jimenez Gomez <erjimene@unal.edu.co>
2020-08-30 03:02:06 -0500

    Termine21

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -81,13 +81,13 @@ public class TV {
 	}
 	
 	public void canalUp() {
-		if (estado == true && canal>=1 && canal<120) {
+		if (estado == true && canal>=1 && canal<=120) {
 			canal++;
 		}		
 	}
 	
 	public void canalDown() {
-		if (estado == true && canal>1 && canal<=120) {
+		if (estado == true && canal>=1 && canal<=120) {
 			canal--;
 		}		
 	}	
